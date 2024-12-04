13bc85ee9d307a7b2f368b5dc44b704506ceb3cb
bbungjin <leejintwo12@naver.com>
2024-12-04 20:44:42 +0900

    refactor: admin 계정 리펙토링

--- src/main/java/com/example/member/initializer/AdminInitializer.java
+++ src/main/java/com/example/member/initializer/AdminInitializer.java
@@ -6,6 +6,7 @@ import com.example.member.dto.request.RegisterRequest;
 import com.example.member.entity.Member;
 import com.example.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -16,12 +17,18 @@ public class AdminInitializer {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Value("${admin.email}")
+    private String adminEmail;
+
+    @Value("${admin.password}")
+    private String adminPassword;
+
     @Bean
     public void init() {
-        if (memberRepository.findByEmail("meowmungg@gmail.com").isEmpty()) {
+        if (memberRepository.findByEmail(adminEmail).isEmpty()) {
             Member member = Member.builder()
-                    .email("meowmungg@gmail.com")
-                    .password(passwordEncoder.encode("qwer1234!@#$"))
+                    .email(adminEmail)
+                    .password(passwordEncoder.encode(adminPassword))
                     .nickname("관리자냥")
                     .loginType(LoginType.JWT)
                     .memberRole(MemberRole.ADMIN)
